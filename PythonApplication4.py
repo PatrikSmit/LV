426e4cb83a882cab0c2b728e4e845df8c5f69742
PatrikSmit <133951086+PatrikSmit@users.noreply.github.com>
2026-03-19 09:57:42 +0100

    Add files via upload

---
+++ PythonApplication4.py
@@ -0,0 +1,14 @@
+﻿import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([1.0, 2.0, 3.0, 3.0, 1.0])
+y = np.array([1.0, 2.0, 2.0, 1.0, 1.0])
+
+plt.figure(figsize=(6,5))
+
+plt.plot(x, y, color='blue', marker='o', linestyle='-', linewidth=2)
+plt.axis([0,4,0,4])
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('zadatak1')
+plt.show()
